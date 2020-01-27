d1301f4da490ba0dbd46599f588ce58a97ab02a6
Relsell <relsellglobal@gmail.com>
2020-01-27 15:21:28 +0530

    added promises

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
-const geocode = require('./geocode/geocode.js');
-const weather = require('./weather/weather.js');
+const geocode = require("./geocode/geocode.js");
+const weather = require("./weather/weather.js");
 
+var startProcess = argv => {
+   var p = getGeoCoding(argv);
+   p
+   .then((results)=>{
+      return getWeather(argv,results);
+   })
+   .then((result)=>{
+      console.log(result);
+   })
+   .catch((err)=>{
 
+   })
+};
 
+var getGeoCoding = argv => {
+  return new Promise((resolve, reject) => {
+    geocode.geocodeAddress(argv.a, argv.gapi_key, (errorMessage, results) => {
+      if (errorMessage) {
+        reject(errorMessage);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
 
-var startProcess = (argv) =>{
-  geocode.geocodeAddress (argv.a,argv.gapi_key,(errorMessage,results)=>{
-    if(errorMessage) {
-      console.log(errorMessage);
-    } else {
-      weather.getWeather(argv.dapi_key,results.latitude,results.longitude,(errorMessage,weatherResults)=>{
-         if(errorMessage) {
-           console.log(errorMessage);
-         } else {
-           console.log(JSON.stringify(weatherResults,undefined,4));
-         }
-       });
-    }
+var getWeather = (argv, results) => {
+  return new Promise((resolve, reject) => {
+    weather.getWeather(
+      argv.dapi_key,
+      results.latitude,
+      results.longitude,
+      (errorMessage, weatherResults) => {
+        if (errorMessage) {
+          reject(errorMessage);
+        } else {
+          resolve(weatherResults);
+        }
+      }
+    );
   });
 };
 
 module.exports = {
-  startProcess
-}
-
-
-
+  startProcess,
+  getWeather,
+  getGeoCoding
+};
